9c5081bebce7686066de2b3c5c3197d9e1d490cb
yangfuhai <fuhai999@gmail.com>
2017-06-05 15:13:40 +0800

    add hystrix support

--- src/main/java/io/jboot/core/metrics/JbootMetricsManager.java
+++ src/main/java/io/jboot/core/metrics/JbootMetricsManager.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.jboot.metrics;
+package io.jboot.core.metrics;
 
 import io.jboot.utils.ClassNewer;
 
