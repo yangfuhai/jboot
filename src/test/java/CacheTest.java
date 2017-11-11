ef25030c942b92d6a74b642a209ed82d949293ee
yangfuhai <fuhai999@gmail.com>
2017-11-11 16:15:36 +0800

    add：cache test

---
+++ src/test/java/CacheTest.java
@@ -0,0 +1,31 @@
+import io.jboot.Jboot;
+import org.junit.Test;
+
+/**
+ * @author Michael Yang 杨福海 （fuhai999@gmail.com）
+ * @version V1.0
+ */
+public class CacheTest {
+
+
+    @Test
+    public void testSetAndGet() {
+        Jboot.setBootArg("jboot.cache.type", "ehcache");
+        Jboot.me().getCache().put("test", "mykey", "abc");
+        System.out.println((String) Jboot.me().getCache().get("test", "mykey"));
+    }
+
+    @Test
+    public void testSetInTimeAndGet() {
+        Jboot.setBootArg("jboot.cache.type", "ehcache");
+        Jboot.me().getCache().put("test", "mykey", "abc", 5);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + " : " + (String) Jboot.me().getCache().get("test", "mykey"));
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
