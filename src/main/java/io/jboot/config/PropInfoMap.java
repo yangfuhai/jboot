42ae3f3dc52a7185838bb341773606c51658225f
yangfuhai <fuhai999@gmail.com>
2017-11-16 10:11:20 +0800

    重构 配置中心 模块

--- src/main/java/io/jboot/config/PropInfoMap.java
+++ src/main/java/io/jboot/config/PropInfoMap.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.jboot.config.server;
+package io.jboot.config;
 
 import com.jfinal.kit.HashKit;
 import com.jfinal.kit.Prop;
