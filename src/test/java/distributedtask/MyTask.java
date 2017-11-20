fde2ded983ea5790117c768f63b60e331783b1a0
yangfuhai <fuhai999@gmail.com>
2017-11-20 23:14:07 +0800

    重构 cron4j 部分

--- src/test/java/distributedtask/MyTask.java
+++ src/test/java/distributedtask/MyTask.java
@@ -1,7 +1,7 @@
 package distributedtask;
 
 import io.jboot.schedule.JbootDistributedRunnable;
-import io.jboot.schedule.annotation.Cron4jTask;
+import io.jboot.schedule.annotation.Cron;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -13,11 +13,11 @@ import java.util.Date;
  * @Description: 每2分钟执行一次
  * @Package distributedtask
  */
-@Cron4jTask(cron = "*/2 * * * *")
+@Cron("*/2 * * * *")
 public class MyTask extends JbootDistributedRunnable {
 
     @Override
-    public boolean run() {
+    public boolean execute() {
         System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         return true;
     }
