eef83bc1e98bd73c83363359a162c6930075a754
yangfuhai <fuhai999@gmail.com>
2017-12-04 20:22:51 +0800

    update sharding-jdbc version to 2.0.0

--- src/main/java/io/jboot/db/datasource/DataSourceBuilder.java
+++ src/main/java/io/jboot/db/datasource/DataSourceBuilder.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * <p>
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,14 +15,14 @@
  */
 package io.jboot.db.datasource;
 
-import com.dangdang.ddframe.rdb.sharding.api.ShardingDataSourceFactory;
-import com.dangdang.ddframe.rdb.sharding.api.rule.ShardingRule;
 import com.jfinal.log.Log;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import io.jboot.db.sharding.IShardingRuleFactory;
 import io.jboot.db.sharding.ShardingRuleFactoryBuilder;
 import io.jboot.utils.StringUtils;
+import io.shardingjdbc.core.jdbc.core.datasource.ShardingDataSource;
+import io.shardingjdbc.core.rule.ShardingRule;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
@@ -75,7 +75,8 @@ public class DataSourceBuilder {
 
         ShardingRule shardingRule = factory.createShardingRule(dataSource);
         try {
-            return ShardingDataSourceFactory.createDataSource(shardingRule);
+//            return ShardingDataSourceFactory.createDataSource(shardingRule);
+            return new ShardingDataSource(shardingRule);
         } catch (SQLException e) {
             log.error("create sharding datasource error." + e.toString(), e);
         }
--- src/main/java/io/jboot/db/sharding/IShardingRuleFactory.java
+++ src/main/java/io/jboot/db/sharding/IShardingRuleFactory.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * <p>
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,7 +15,8 @@
  */
 package io.jboot.db.sharding;
 
-import com.dangdang.ddframe.rdb.sharding.api.rule.ShardingRule;
+
+import io.shardingjdbc.core.rule.ShardingRule;
 
 import javax.sql.DataSource;
 
