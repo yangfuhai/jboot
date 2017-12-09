3810158a519f13d8474876fd4b18c8f5392234c1
yangfuhai <fuhai999@gmail.com>
2017-12-09 17:47:06 +0800

    add DataSourceFactory config support

--- src/main/java/io/jboot/db/JbootDbManager.java
+++ src/main/java/io/jboot/db/JbootDbManager.java
@@ -20,8 +20,8 @@ import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
 import com.jfinal.plugin.activerecord.Model;
 import io.jboot.Jboot;
 import io.jboot.db.datasource.DataSourceBuilder;
-import io.jboot.db.datasource.DatasourceConfig;
-import io.jboot.db.datasource.DatasourceConfigManager;
+import io.jboot.db.datasource.DataSourceConfig;
+import io.jboot.db.datasource.DataSourceConfigManager;
 import io.jboot.db.dialect.*;
 import io.jboot.exception.JbootException;
 import io.jboot.utils.ArrayUtils;
@@ -52,20 +52,20 @@ public class JbootDbManager {
     public JbootDbManager() {
 
         // 所有的数据源
-        Map<String, DatasourceConfig> datasourceConfigs = DatasourceConfigManager.me().getDatasourceConfigs();
+        Map<String, DataSourceConfig> datasourceConfigs = DataSourceConfigManager.me().getDatasourceConfigs();
 
         // 分库的数据源，一个数据源包含了多个数据源。
-        Map<String, DatasourceConfig> shardingDatasourceConfigs = DatasourceConfigManager.me().getShardingDatasourceConfigs();
+        Map<String, DataSourceConfig> shardingDatasourceConfigs = DataSourceConfigManager.me().getShardingDatasourceConfigs();
 
         if (shardingDatasourceConfigs != null && shardingDatasourceConfigs.size() > 0) {
-            for (Map.Entry<String, DatasourceConfig> entry : shardingDatasourceConfigs.entrySet()) {
+            for (Map.Entry<String, DataSourceConfig> entry : shardingDatasourceConfigs.entrySet()) {
                 String databaseConfig = entry.getValue().getShardingDatabase();
                 if (StringUtils.isBlank(databaseConfig)) {
                     continue;
                 }
                 Set<String> databases = StringUtils.splitToSet(databaseConfig, ",");
                 for (String database : databases) {
-                    DatasourceConfig datasourceConfig = datasourceConfigs.remove(database);
+                    DataSourceConfig datasourceConfig = datasourceConfigs.remove(database);
                     if (datasourceConfig == null) {
                         throw new NullPointerException("has no datasource config named " + database + ",plase check your sharding database config");
                     }
@@ -75,7 +75,7 @@ public class JbootDbManager {
         }
 
         //所有数据源，包含了分库的和未分库的
-        Map<String, DatasourceConfig> allDatasourceConfigs = new HashMap<>();
+        Map<String, DataSourceConfig> allDatasourceConfigs = new HashMap<>();
         if (datasourceConfigs != null) {
             allDatasourceConfigs.putAll(datasourceConfigs);
         }
@@ -85,9 +85,9 @@ public class JbootDbManager {
         }
 
 
-        for (Map.Entry<String, DatasourceConfig> entry : allDatasourceConfigs.entrySet()) {
+        for (Map.Entry<String, DataSourceConfig> entry : allDatasourceConfigs.entrySet()) {
 
-            DatasourceConfig datasourceConfig = entry.getValue();
+            DataSourceConfig datasourceConfig = entry.getValue();
 
             if (datasourceConfig.isConfigOk()) {
 
@@ -110,7 +110,7 @@ public class JbootDbManager {
      * @param datasourceConfig
      * @param activeRecordPlugin
      */
-    private void configSqlTemplate(DatasourceConfig datasourceConfig, ActiveRecordPlugin activeRecordPlugin) {
+    private void configSqlTemplate(DataSourceConfig datasourceConfig, ActiveRecordPlugin activeRecordPlugin) {
         String sqlTemplatePath = datasourceConfig.getSqlTemplatePath();
         if (sqlTemplatePath != null) {
             if (sqlTemplatePath.startsWith("/")) {
@@ -135,24 +135,24 @@ public class JbootDbManager {
      * @param activeRecordPlugin
      * @param datasourceConfig
      */
-    private void configDialect(ActiveRecordPlugin activeRecordPlugin, DatasourceConfig datasourceConfig) {
+    private void configDialect(ActiveRecordPlugin activeRecordPlugin, DataSourceConfig datasourceConfig) {
         switch (datasourceConfig.getType()) {
-            case DatasourceConfig.TYPE_MYSQL:
+            case DataSourceConfig.TYPE_MYSQL:
                 activeRecordPlugin.setDialect(new JbootMysqlDialect());
                 break;
-            case DatasourceConfig.TYPE_ORACLE:
+            case DataSourceConfig.TYPE_ORACLE:
                 activeRecordPlugin.setDialect(new JbootOracleDialect());
                 break;
-            case DatasourceConfig.TYPE_SQLSERVER:
+            case DataSourceConfig.TYPE_SQLSERVER:
                 activeRecordPlugin.setDialect(new JbootSqlServerDialect());
                 break;
-            case DatasourceConfig.TYPE_SQLITE:
+            case DataSourceConfig.TYPE_SQLITE:
                 activeRecordPlugin.setDialect(new JbootSqlite3Dialect());
                 break;
-            case DatasourceConfig.TYPE_ANSISQL:
+            case DataSourceConfig.TYPE_ANSISQL:
                 activeRecordPlugin.setDialect(new JbootAnsiSqlDialect());
                 break;
-            case DatasourceConfig.TYPE_POSTGRESQL:
+            case DataSourceConfig.TYPE_POSTGRESQL:
                 activeRecordPlugin.setDialect(new JbootPostgreSqlDialect());
                 break;
             default:
@@ -167,7 +167,7 @@ public class JbootDbManager {
      * @param config
      * @return
      */
-    private ActiveRecordPlugin createRecordPlugin(DatasourceConfig config) {
+    private ActiveRecordPlugin createRecordPlugin(DataSourceConfig config) {
 
         String configName = config.getName();
         DataSource dataSource = new DataSourceBuilder(config).build();
---
+++ src/main/java/io/jboot/db/datasource/HikariDataSourceFactory.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2015-2017, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.jboot.db.datasource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Michael Yang 杨福海 （fuhai999@gmail.com）
+ * @version V1.0
+ * @Package io.jboot.db.datasource
+ */
+public class HikariDataSourceFactory implements DataSourceFactory {
+
+    @Override
+    public DataSource createDataSource(DataSourceConfig dataSourceConfig) {
+
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(dataSourceConfig.getUrl());
+        hikariConfig.setUsername(dataSourceConfig.getUser());
+        hikariConfig.setPassword(dataSourceConfig.getPassword());
+        hikariConfig.addDataSourceProperty("cachePrepStmts", dataSourceConfig.isCachePrepStmts());
+        hikariConfig.addDataSourceProperty("prepStmtCacheSize", dataSourceConfig.getPrepStmtCacheSize());
+        hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", dataSourceConfig.getPrepStmtCacheSqlLimit());
+
+        hikariConfig.setDriverClassName(dataSourceConfig.getDriverClassName());
+        hikariConfig.setPoolName(dataSourceConfig.getPoolName());
+
+
+        if (hikariConfig.getConnectionInitSql() != null) {
+            hikariConfig.setConnectionInitSql(dataSourceConfig.getConnectionInitSql());
+        }
+
+
+        hikariConfig.setMaximumPoolSize(dataSourceConfig.getMaximumPoolSize());
+
+        return new HikariDataSource(hikariConfig);
+    }
+}
