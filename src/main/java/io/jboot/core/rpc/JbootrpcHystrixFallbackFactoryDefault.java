ea72e467a4d1ed650c2ed729d0a25e52fa1f43d1
yangfuhai <fuhai999@gmail.com>
2017-12-09 09:10:25 +0800

    jboot v1.1.3 release

--- src/main/java/io/jboot/core/rpc/JbootrpcHystrixFallbackFactoryDefault.java
+++ src/main/java/io/jboot/core/rpc/JbootrpcHystrixFallbackFactoryDefault.java
@@ -15,6 +15,7 @@
  */
 package io.jboot.core.rpc;
 
+import com.netflix.hystrix.exception.HystrixTimeoutException;
 import io.jboot.component.hystrix.JbootHystrixCommand;
 
 import java.lang.reflect.Method;
@@ -24,6 +25,11 @@ public class JbootrpcHystrixFallbackFactoryDefault implements JbootrpcHystrixFal
 
     @Override
     public Object fallback(Object proxy, Method method, Object[] args, JbootHystrixCommand command, Throwable exception) {
+        if (exception instanceof HystrixTimeoutException) {
+            System.err.println("rpc request timeout，the timeout time is defalut 3000 milliseconds，" +
+                    "you can config jboot.rpc.hystrixTimeout to set the value." +
+                    "or config \"jboot.rpc.hystrixEnable = false\" to close hystrix.");
+        }
         exception.printStackTrace();
         return null;
     }
