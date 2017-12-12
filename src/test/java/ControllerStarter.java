58096a6a06d8e69991056b2b740040a0a8d3fbef
yangfuhai <fuhai999@gmail.com>
2017-12-12 09:11:55 +0800

    jboot v1.1.6 release

--- src/test/java/ControllerStarter.java
+++ src/test/java/ControllerStarter.java
@@ -8,8 +8,9 @@ public class ControllerStarter {
         Jboot.setBootArg("jboot.hystrix.url", "/hystrix.stream");
         Jboot.setBootArg("jboot.cache.type", "redis");
         Jboot.setBootArg("jboot.cache.redis.host", "127.0.0.1");
-        Jboot.run(args);
 
+
+        Jboot.run(args);
 
 
     }
