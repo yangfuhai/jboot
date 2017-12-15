2382a00b7ff1f268a7c7aa80684ca2b81a3f6276
yangfuhai <fuhai999@gmail.com>
2017-12-15 15:11:09 +0800

    add columns implements Serializable

--- src/main/java/io/jboot/db/model/Column.java
+++ src/main/java/io/jboot/db/model/Column.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * <p>
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,7 +16,9 @@
 package io.jboot.db.model;
 
 
-public class Column {
+import java.io.Serializable;
+
+public class Column implements Serializable {
     public static final String LOGIC_LIKE = " LIKE ";
     public static final String LOGIC_GT = " > ";
     public static final String LOGIC_GE = " >= ";
