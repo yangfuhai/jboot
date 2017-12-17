3025c38926d77b234726c642bcd91939ecf33177
yangfuhai <fuhai999@gmail.com>
2017-12-17 15:20:32 +0800

    jboot v1.1.9 release

--- src/main/java/io/jboot/JbootConfig.java
+++ src/main/java/io/jboot/JbootConfig.java
@@ -22,7 +22,7 @@ import io.jboot.web.JbootAppConfig;
 public class JbootConfig {
 
 
-    private String version = "v1.1.9";
+    private String version = "1.1.9";
     private String mode = Jboot.MODE.DEV.getValue();
     private boolean bannerEnable = true;
     private String bannerFile = "banner.txt";
