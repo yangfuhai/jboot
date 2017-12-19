7d677c7d253429258e91dc7cd1d2239b98199f51
yangfuhai <fuhai999@gmail.com>
2017-12-20 00:30:46 +0800

    set jboot default cache is ehcache

--- src/main/java/io/jboot/core/cache/JbootCacheConfig.java
+++ src/main/java/io/jboot/core/cache/JbootCacheConfig.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * <p>
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -25,10 +25,10 @@ public class JbootCacheConfig {
     public static final String TYPE_EHCACHE = "ehcache";
     public static final String TYPE_REDIS = "redis";
     public static final String TYPE_EHREDIS = "ehredis";
-    public static final String TYPE_NONE_CACHE = "nonecache";
+    public static final String TYPE_NONE_CACHE = "none";
 
 
-    private String type = TYPE_NONE_CACHE;
+    private String type = TYPE_EHCACHE;
 
     public String getType() {
         return type;
