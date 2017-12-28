e01e64296e8493dcbbded80b534ebbb9e5a93fb9
yangfuhai <fuhai999@gmail.com>
2017-12-28 09:27:34 +0800

    优化 spiloader 可能出现的空指针异常的问题

--- src/main/java/io/jboot/core/spi/JbootSpiLoader.java
+++ src/main/java/io/jboot/core/spi/JbootSpiLoader.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * <p>
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -47,13 +47,14 @@ public class JbootSpiLoader {
             T t = iterator.next();
             if (spiName == null) {
                 return t;
-            } else {
-                JbootSpi spi = t.getClass().getAnnotation(JbootSpi.class);
-                if (spiName.equals(spi.value())) {
-                    return t;
-                }
             }
-
+            JbootSpi spi = t.getClass().getAnnotation(JbootSpi.class);
+            if (spi == null) {
+                continue;
+            }
+            if (spiName.equals(spi.value())) {
+                return t;
+            }
         }
         return null;
     }
