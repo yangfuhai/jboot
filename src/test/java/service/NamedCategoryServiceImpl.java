b275a90d20081d872ae7287289b2cfe870691c14
yangfuhai <fuhai999@gmail.com>
2017-12-28 15:59:23 +0800

    AOP 添加 @Bean(name="xxx") 的支持，方便实现一个接口有多个实现类的情况

--- src/test/java/service/NamedCategoryServiceImpl.java
+++ src/test/java/service/NamedCategoryServiceImpl.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * <p>
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,14 +15,13 @@
  */
 package service;
 
-
 import io.jboot.aop.annotation.Bean;
 
-@Bean
-public class CategoryServiceImpl implements CategoryService {
+@Bean(name = "myCategory")
+public class NamedCategoryServiceImpl implements CategoryService {
     @Override
     public String hello(String text) {
-        return "CategoryServiceImpl say hello " + text;
+        return "NamedCategoryServiceImpl say hello " + text;
     }
 
 }
