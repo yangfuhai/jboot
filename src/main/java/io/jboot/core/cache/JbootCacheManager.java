beeae485ab895a469dcbc89d777c977724493875
yangfuhai <fuhai999@gmail.com>
2017-12-29 14:31:48 +0800

    add j2cache support

--- src/main/java/io/jboot/core/cache/JbootCacheManager.java
+++ src/main/java/io/jboot/core/cache/JbootCacheManager.java
@@ -18,6 +18,7 @@ package io.jboot.core.cache;
 import io.jboot.Jboot;
 import io.jboot.core.cache.ehcache.JbootEhcacheImpl;
 import io.jboot.core.cache.ehredis.JbootEhredisCacheImpl;
+import io.jboot.core.cache.j2cache.J2cacheImpl;
 import io.jboot.core.cache.redis.JbootRedisCacheImpl;
 import io.jboot.core.spi.JbootSpiLoader;
 
@@ -38,19 +39,19 @@ public class JbootCacheManager {
     public JbootCache getCache() {
         if (jbootCache == null) {
             JbootCacheConfig config = Jboot.config(JbootCacheConfig.class);
-            jbootCache = getCache(config);
+            jbootCache = buildCache(config);
         }
         return jbootCache;
     }
 
-    public JbootCache getCache(JbootCacheConfig cacheConfig) {
+    public JbootCache getCache(String type) {
+        JbootCacheConfig cacheConfig = new JbootCacheConfig();
+        cacheConfig.setType(type);
         return buildCache(cacheConfig);
     }
 
     private JbootCache buildCache(JbootCacheConfig config) {
-        if (config == null) {
-            throw new IllegalArgumentException("config must not be null");
-        }
+
         switch (config.getType()) {
             case JbootCacheConfig.TYPE_EHCACHE:
                 return new JbootEhcacheImpl();
@@ -60,6 +61,8 @@ public class JbootCacheManager {
                 return new JbootEhredisCacheImpl();
             case JbootCacheConfig.TYPE_NONE_CACHE:
                 return new NoneCacheImpl();
+            case JbootCacheConfig.TYPE_J2CACHE:
+                return new J2cacheImpl();
             default:
                 return JbootSpiLoader.load(JbootCache.class, config.getType());
         }
