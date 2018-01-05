fe557f8a61cd5285878a2ed245f4d0ddd3923016
xhf <xhf6731202@126.com>
2018-01-05 17:54:06 +0800

    修改copyright

--- src/test/java/mqqpid/QpidMqServerDemo.java
+++ src/test/java/mqqpid/QpidMqServerDemo.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015-2018, 徐海峰 (27533892@qq.com).
+ * Copyright (c) 2015-2018, Michael Yang 杨福海 (fuhai999@gmail.com).
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
