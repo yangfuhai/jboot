5b9a5bf1e61bf230781d1c76e11747545f6e07ba
yangfuhai <fuhai999@gmail.com>
2018-02-06 14:27:06 +0800

    reconstruction JFinal Before invocation

--- src/main/java/io/jboot/aop/interceptor/JFinalBeforeInvocation.java
+++ src/main/java/io/jboot/aop/interceptor/JFinalBeforeInvocation.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * <p>
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -46,7 +46,11 @@ public class JFinalBeforeInvocation extends Invocation {
             try {
                 setReturnValue(methodInvocation.proceed());
             } catch (Throwable throwable) {
-                throw new JbootException(throwable);
+                if (throwable instanceof RuntimeException) {
+                    throw (RuntimeException) throwable;
+                } else {
+                    throw new JbootException(throwable.getMessage(), throwable);
+                }
             }
         }
     }
