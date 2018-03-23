d59597f3ccee1b84ec1072d4f0d12f782282e6ea
yangfuhai <fuhai999@gmail.com>
2018-03-23 11:25:06 +0800

    fixed：修复 RPC 由于默认排除是Object导致无法暴露的问题

---
+++ src/test/java/service/MyUser.java
@@ -0,0 +1,11 @@
+package service;
+
+/**
+ * @author Michael Yang 杨福海 （fuhai999@gmail.com）
+ * @version V1.0
+ * @Title: (请输入文件名称)
+ * @Description: (用一句话描述该文件做什么)
+ * @Package service
+ */
+public class MyUser extends User {
+}
