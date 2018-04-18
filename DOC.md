52cd4805d4c7269bc8563329b7ca8bdcce4788a9
yangfuhai <fuhai999@gmail.com>
2018-04-18 09:26:14 +0800

    jboot v1.4.7 release

--- DOC.md
+++ DOC.md
@@ -111,7 +111,7 @@
 <dependency>
     <groupId>io.jboot</groupId>
     <artifactId>jboot</artifactId>
-    <version>1.4.6</version>
+    <version>1.4.7</version>
 </dependency>
 ```
 #### 编写helloworld
--- src/main/java/io/jboot/JbootConfig.java
+++ src/main/java/io/jboot/JbootConfig.java
@@ -22,7 +22,7 @@ import io.jboot.web.JbootAppConfig;
 public class JbootConfig {
 
 
-    private String version = "1.4.6";
+    private String version = "1.4.7";
     private String mode = Jboot.MODE.DEV.getValue();
     private boolean bannerEnable = true;
     private String bannerFile = "banner.txt";
