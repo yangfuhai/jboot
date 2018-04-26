cd20f4e26232c7c4a02d98cd23dd0d9e364c50b2
retire <wyf372310383@163.com>
2018-04-26 14:23:47 +0800

    修复 redis连接超时问题

--- src/main/java/io/jboot/component/redis/jedis/JbootJedisImpl.java
+++ src/main/java/io/jboot/component/redis/jedis/JbootJedisImpl.java
@@ -82,6 +82,22 @@ public class JbootJedisImpl extends JbootRedisBase {
             poolConfig.setNumTestsPerEvictionRun(config.getNumTestsPerEvictionRun());
         }
 
+        if (StringUtils.isNotBlank(config.getMaxTotal())) {
+            poolConfig.setMaxTotal(config.getMaxTotal());
+        }
+
+        if (StringUtils.isNotBlank(config.getMaxIdle())) {
+            poolConfig.setMaxIdle(config.getMaxIdle());
+        }
+
+        if (StringUtils.isNotBlank(config.getMinIdle())) {
+            poolConfig.setMinIdle(config.getMinIdle());
+        }
+
+        if (StringUtils.isNotBlank(config.getMaxWaitMillis())) {
+            poolConfig.setMaxWaitMillis(config.getMaxWaitMillis());
+        }
+
         this.jedisPool = newJedisPool(poolConfig, host, port, timeout, password, database, clientName);
 
     }
