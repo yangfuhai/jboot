a28a2eb6ef25e789c3ae5a802e291cc43e91aba7
yangfuhai <fuhai999@gmail.com>
2018-05-09 17:13:48 +0800

    optimize LimitationControllerInter

--- src/main/java/io/jboot/web/limitation/web/LimitationControllerInter.java
+++ src/main/java/io/jboot/web/limitation/web/LimitationControllerInter.java
@@ -30,14 +30,10 @@ import io.jboot.web.limitation.LimitationConfig;
 public class LimitationControllerInter implements Interceptor {
 
 
-    private static Authorizer authorizer;
-
     @Override
     public void intercept(Invocation inv) {
 
-        initAuthorizer();
-
-        if (!authorizer.onAuthorize(inv.getController())) {
+        if (!getAuthorizer().onAuthorize(inv.getController())) {
             inv.getController().renderError(404);
             return;
         }
@@ -45,16 +41,19 @@ public class LimitationControllerInter implements Interceptor {
         inv.invoke();
     }
 
-    private void initAuthorizer() {
-        if (authorizer != null) {
-            return;
+    private static Authorizer authorizer;
+
+    private Authorizer getAuthorizer() {
+
+        if (authorizer == null) {
+            String authorizerClass = Jboot.config(LimitationConfig.class).getWebAuthorizer();
+            authorizer = ClassKits.newInstance(authorizerClass);
+            if (authorizer == null) {
+                throw new JbootException("can not init authorizer for class : " + authorizerClass);
+            }
         }
 
-        String authorizerClass = Jboot.config(LimitationConfig.class).getWebAuthorizer();
-        authorizer = ClassKits.newInstance(authorizerClass);
-        if (authorizer == null) {
-            throw new JbootException("can not init authorizer for class : " + authorizerClass);
-        }
+        return authorizer;
     }
 
 
