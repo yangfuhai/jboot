68a1aa80a498df35d5b7e4fd750b506755302f1f
yangfuhai <fuhai999@gmail.com>
2018-05-22 08:42:46 +0800

    optimize  shiromanager

--- src/main/java/io/jboot/component/shiro/JbootShiroManager.java
+++ src/main/java/io/jboot/component/shiro/JbootShiroManager.java
@@ -56,6 +56,9 @@ public class JbootShiroManager {
 
 
     public void init(List<Routes.Route> routes) {
+        if (!jbootShiroConfig.isConfigOK()) {
+            return;
+        }
         initInvokers(routes);
     }
 
