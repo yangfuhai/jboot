04a399a492e0b9ce1deb306f6f4ace5398ad3e73
retireworker <wyf372310383@163.com>
2018-11-01 00:30:05 +0800

    get rpc config test

--- src/test/java/rpcconfig/DubboServerConfigDemo.java
+++ src/test/java/rpcconfig/DubboServerConfigDemo.java
@@ -1,4 +1,4 @@
-package rpczookeeper;
+package rpcconfig;
 
 import io.jboot.Jboot;
 import io.jboot.core.rpc.JbootrpcServiceConfig;
@@ -6,7 +6,7 @@ import io.jboot.core.rpc.JbootrpcServiceConfig;
 /**
  * Created by Administrator on 2018/11/1.
  */
-public class DubboServer3ZookeeperDemo {
+public class DubboServerConfigDemo {
 
         public static void main(String[] args) throws InterruptedException {
 
@@ -17,6 +17,7 @@ public class DubboServer3ZookeeperDemo {
                 Jboot.setBootArg("jboot.rpc.callMode", "registry");//注册中心模式
                 Jboot.setBootArg("jboot.rpc.registryType", "zookeeper");//注册中心的类型：zookeeper
                 Jboot.setBootArg("jboot.rpc.registryAddress", "127.0.0.1:2181");//注册中心，即zookeeper的地址
+                //自定义配置
                 Jboot.setBootArg("jboot.rpc.proxy","javassist");
                 Jboot.setBootArg("jboot.rpc.filter","cache");
 
