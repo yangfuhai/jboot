da1e3591ed905e328addf755720128fe8c5cba7c
yangfuhai <fuhai999@gmail.com>
2018-12-18 15:02:53 +0800

    jboot v2.0 init

--- src/main/java/io/jboot/aop/JbootInjectManager.java
+++ src/main/java/io/jboot/aop/JbootInjectManager.java
@@ -1,5 +1,5 @@
 ///**
-// * Copyright (c) 2015-2018, Michael Yang 杨福海 (fuhai999@gmail.com).
+// * Copyright (c) 2015-2019, Michael Yang 杨福海 (fuhai999@gmail.com).
 // * <p>
 // * Licensed under the Apache License, Version 2.0 (the "License");
 // * you may not use this file except in compliance with the License.
