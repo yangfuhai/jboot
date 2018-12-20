e65c8ed774c0757886da902c894e1821a4893da6
yangfuhai <fuhai999@gmail.com>
2018-12-20 13:40:00 +0800

    jboot v2.0 init

--- src/test/java/io/jboot/rpc/dubbo/DubboServer.java
+++ src/test/java/io/jboot/rpc/dubbo/DubboServer.java
@@ -9,7 +9,7 @@ public class DubboServer {
 
 
         JbootApplication.setBootArg("jboot.rpc.type", "dubbo");
-        JbootApplication.setBootArg("jboot.rpc.callMode", "redirect");//直连模式，默认为注册中心
+        JbootApplication.setBootArg("jboot.rpc.callMode", "direct");//直连模式，默认为注册中心
         JbootApplication.setBootArg("jboot.rpc.directUrl", "127.0.0.1:8000");//直连模式的url地址
 
         JbootApplication.run(args);
