8a22a37b0051572db125c089939728f799def299
yangfuhai <fuhai999@gmail.com>
2018-12-21 12:38:20 +0800

    jboot v2.0 init

---
+++ src/test/java/io/jboot/mq/redis/RedisMqReceiver.java
@@ -0,0 +1,34 @@
+package io.jboot.mq.redis;
+
+
+import io.jboot.Jboot;
+import io.jboot.app.JbootApplication;
+
+public class RedisMqReceiver {
+
+    public static void main(String[] args) {
+
+        //Undertow端口号配置
+        JbootApplication.setBootArg("undertow.port", "8001");
+
+        //设置 mq 的相关信息
+        JbootApplication.setBootArg("jboot.mq.type", "redis");
+        JbootApplication.setBootArg("jboot.mq.channel", "channel1,channel2,myChannel");
+        JbootApplication.setBootArg("jboot.mq.redis.host", "127.0.0.1");
+
+        //启动应用程序
+        JbootApplication.run(args);
+
+        //添加监听
+        Jboot.getMq().addMessageListener((channel, message) -> {
+            System.out.println("listener1 receive msg : " + message + ", from channel : " + channel);
+        });
+
+        // 只监听 myChannel 这个通道
+        Jboot.getMq().addMessageListener((channel, message) -> {
+            System.out.println("listener2 receive msg : " + message + ", from channel : " + channel);
+        },"myChannel");
+
+        System.out.println("RedisMqReceiver started.");
+    }
+}
---
+++ src/test/java/io/jboot/mq/redis/RedisMqSender.java
@@ -0,0 +1,34 @@
+package io.jboot.mq.redis;
+
+
+import io.jboot.Jboot;
+import io.jboot.app.JbootApplication;
+
+public class RedisMqSender {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //Undertow端口号配置
+        JbootApplication.setBootArg("undertow.port", "8000");
+
+        //设置 mq 的相关信息
+        JbootApplication.setBootArg("jboot.mq.type", "redis");
+        JbootApplication.setBootArg("jboot.mq.redis.host", "127.0.0.1");
+
+        //启动应用程序
+        JbootApplication.run(args);
+
+        while (true) {
+
+            Jboot.getMq().publish("message from RedisMqSender","channel1");
+            Jboot.getMq().publish("message from RedisMqSender","channel2");
+            Jboot.getMq().publish("message from RedisMqSender","myChannel");
+
+            Thread.sleep(2000);
+            System.out.println("jboot mq publish success...");
+        }
+
+    }
+
+
+}
