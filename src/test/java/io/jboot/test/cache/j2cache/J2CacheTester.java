a712cfce618c5787fb453b255e6905922da04fd5
yangfuhai <fuhai999@gmail.com>
2018-12-22 14:45:39 +0800

    jboot 2.0 init

---
+++ src/test/java/io/jboot/test/cache/j2cache/J2CacheTester.java
@@ -0,0 +1,31 @@
+package io.jboot.test.cache.j2cache;
+
+
+import io.jboot.Jboot;
+import io.jboot.app.JbootApplication;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class J2CacheTester {
+
+    @Test
+    public void testPut() {
+        Jboot.getCache().put("cachename", "key", "value");
+        String value = Jboot.getCache().get("cachename", "key");
+        Assert.assertNotNull(value);
+    }
+
+    @Test
+    public void testGet() {
+        Jboot.getCache().put("cachename", "key", "value");
+        String value = Jboot.getCache().get("cachename", "key");
+        Assert.assertTrue("value".equals(value));
+    }
+
+
+    @Before
+    public void config() {
+        JbootApplication.setBootArg("jboot.cache.type", "j2cache");
+    }
+}
---
+++ src/test/java/io/jboot/test/cache/redis/RedisCacheTester.java
@@ -0,0 +1,41 @@
+package io.jboot.test.cache.redis;
+
+
+import io.jboot.Jboot;
+import io.jboot.app.JbootApplication;
+import io.jboot.components.cache.redis.JbootRedisCacheImpl;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RedisCacheTester {
+
+
+    @Test
+    public void testCacheType() {
+        Assert.assertTrue(Jboot.getCache().getClass() == JbootRedisCacheImpl.class);
+    }
+
+
+    @Test
+    public void testPut() {
+        Jboot.getCache().put("cachename", "key", "value");
+        String value = Jboot.getCache().get("cachename", "key");
+        Assert.assertNotNull(value);
+    }
+
+    @Test
+    public void testGet() {
+        Jboot.getCache().put("cachename", "key", "value");
+        String value = Jboot.getCache().get("cachename", "key");
+        Assert.assertTrue("value".equals(value));
+    }
+
+
+    @Before
+    public void config() {
+        JbootApplication.setBootArg("jboot.cache.type", "redis");
+        JbootApplication.setBootArg("jboot.cache.redis.host", "127.0.0.1");
+        JbootApplication.setBootArg("jboot.cache.redis.port", "6379");
+    }
+}
