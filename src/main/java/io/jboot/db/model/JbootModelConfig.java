615a01d3208a5d6bab74a5eb72f7bbba0409c430
yangfuhai <fuhai999@gmail.com>
2019-01-31 11:00:52 +0800

    v2.0-rc.4

--- src/main/java/io/jboot/db/model/JbootModelConfig.java
+++ src/main/java/io/jboot/db/model/JbootModelConfig.java
@@ -33,7 +33,7 @@ public class JbootModelConfig {
 
     private String columnCreated = "created";
     private String columnModified = "modified";
-    private int idCacheTime = 60 * 60 * 24 * 2; // id 缓存默认缓存2天的时间
+    private int idCacheTime = 60 * 60 * 1; // id 缓存默认缓存1个小时
     private boolean idCacheEnable = true; // 是否启用ID自动缓存
     private String idCacheType = Jboot.config(JbootCacheConfig.class).getType();
 
@@ -45,7 +45,6 @@ public class JbootModelConfig {
     public void setScan(String scan) {
         this.scan = scan;
     }
-
 
     public String getColumnCreated() {
         return columnCreated;
