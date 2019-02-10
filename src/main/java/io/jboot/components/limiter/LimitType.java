5895087672806ec39351d427d8e8afbb7c9f51c8
yangfuhai <fuhai999@gmail.com>
2019-02-10 10:14:44 +0800

    v2.0-rc.8

--- src/main/java/io/jboot/components/limiter/LimitType.java
+++ src/main/java/io/jboot/components/limiter/LimitType.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * <p>
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,39 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.jboot.components.limiter.annotation;
+package io.jboot.components.limiter;
 
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-@Documented
-public @interface EnableLimit {
+/**
+ * 限制类型
+ */
+public class LimitType {
 
     /**
-     * 资源名称
-     * @return
+     * 令牌桶，通过 guava 的 RateLimiter 来实现
      */
-    String resource();
+    public static final String TOKEN_BUCKET = "tb";
 
     /**
-     * 类型 ：
-     * @return
+     * 并发量，通过 Semaphore 来实现
      */
-    String type();
-
-    /**
-     * 频率
-     * @return
-     */
-    String rate();
-
-
-    /**
-     * 降级
-     * @return
-     */
-    String failback();
-
+    public static final String CONCURRENCY = "cc";
 
 }
