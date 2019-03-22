6ee2e9f1caaf39d7a3a56e8ea8c0bc204294667d
yangfuhai <fuhai999@gmail.com>
2019-03-22 14:17:38 +0800

    add fescar support

--- src/test/java/io/jboot/test/fescar/business/BusinessServiceProvider.java
+++ src/test/java/io/jboot/test/fescar/business/BusinessServiceProvider.java
@@ -2,7 +2,7 @@ package io.jboot.test.fescar.business;
 
 
 import io.jboot.aop.annotation.Bean;
-import io.jboot.components.fescar.annotation.JbootGlobalTransactional;
+import io.jboot.support.fescar.annotation.FescarGlobalTransactional;
 import io.jboot.components.rpc.annotation.RPCInject;
 import io.jboot.service.JbootServiceBase;
 import io.jboot.test.fescar.account.IAccountService;
@@ -16,7 +16,7 @@ public class BusinessServiceProvider extends JbootServiceBase<Account> {
 	@RPCInject
 	private IStockService stockService;
 
-	@JbootGlobalTransactional(timeoutMills = 300000, name = "Dubbo_Fescar_Account_Service")
+	@FescarGlobalTransactional(timeoutMills = 300000, name = "Dubbo_Fescar_Account_Service")
 	public boolean deposit(Integer accountId) {
 		accountService.deposit(accountId, 1000);
 		stockService.deposit(accountId, 2000);
