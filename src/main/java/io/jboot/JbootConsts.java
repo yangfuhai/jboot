6c2f858972f9940d5c9e22fc00ce7759c96ef608
yangfuhai <fuhai999@gmail.com>
2019-05-08 12:01:04 +0800

    v2.0.9 release (^.^)YYa!!

--- src/main/java/io/jboot/JbootConsts.java
+++ src/main/java/io/jboot/JbootConsts.java
@@ -22,7 +22,7 @@ package io.jboot;
  */
 public class JbootConsts {
 
-    public static String VERSION = "2.0.8";
+    public static String VERSION = "2.0.9";
 
 
     public static final String ATTR_REQUEST = "REQUEST";
