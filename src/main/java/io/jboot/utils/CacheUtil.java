bf8bdca20bfae0bcb6b37a3fd9e4ba0d52a3881d
yangfuhai <fuhai999@gmail.com>
2019-05-28 12:20:13 +0800

    add CacheUtil

---
+++ src/main/java/io/jboot/utils/CacheUtil.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2015-2019, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.jboot.utils;
+
+import com.jfinal.plugin.ehcache.IDataLoader;
+import io.jboot.Jboot;
+
+import java.util.List;
+
+
+public class CacheUtil {
+
+    public <T> T get(String cacheName, Object key) {
+        return Jboot.getCache().get(cacheName, key);
+    }
+
+    public void put(String cacheName, Object key, Object value) {
+        Jboot.getCache().put(cacheName, key, value);
+    }
+
+    public void put(String cacheName, Object key, Object value, int liveSeconds) {
+        Jboot.getCache().put(cacheName, key, value, liveSeconds);
+    }
+
+    public List getKeys(String cacheName) {
+        return Jboot.getCache().getKeys(cacheName);
+    }
+
+    public void remove(String cacheName, Object key) {
+        Jboot.getCache().remove(cacheName, key);
+    }
+
+    public void removeAll(String cacheName) {
+        Jboot.getCache().removeAll(cacheName);
+    }
+
+    public <T> T get(String cacheName, Object key, IDataLoader dataLoader) {
+        return Jboot.getCache().get(cacheName, key, dataLoader);
+    }
+
+    public <T> T get(String cacheName, Object key, IDataLoader dataLoader, int liveSeconds) {
+        return Jboot.getCache().get(cacheName, key, dataLoader, liveSeconds);
+    }
+
+    public Integer getTtl(String cacheName, Object key) {
+        return Jboot.getCache().getTtl(cacheName, key);
+    }
+
+    public void setTtl(String cacheName, Object key, int seconds) {
+        Jboot.getCache().setTtl(cacheName, key, seconds);
+    }
+}
