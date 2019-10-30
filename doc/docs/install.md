8ba2cd20a8477a29bcc46ac338b3012d84b04c36
yangfuhai <fuhai999@gmail.com>
2019-10-30 12:05:20 +0800

    v2.2.5 release (^.^)YYa!!

--- doc/docs/install.md
+++ doc/docs/install.md
@@ -9,7 +9,7 @@
 <dependency>
     <groupId>io.jboot</groupId>
     <artifactId>jboot</artifactId>
-    <version>2.2.4</version>
+    <version>2.2.5</version>
 </dependency>
 ```
 
--- src/main/java/io/jboot/JbootConsts.java
+++ src/main/java/io/jboot/JbootConsts.java
@@ -22,7 +22,7 @@ package io.jboot;
  */
 public class JbootConsts {
 
-    public static String VERSION = "2.2.4";
+    public static String VERSION = "2.2.5";
 
 
     public static final String ATTR_REQUEST = "REQUEST";
