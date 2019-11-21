5120ee3a4ecff75176e27bf91f0404432a1f2b06
yangfuhai <fuhai999@gmail.com>
2019-11-21 16:08:15 +0800

    v2.2.8

--- src/main/java/io/jboot/components/cache/annotation/Cacheable.java
+++ src/main/java/io/jboot/components/cache/annotation/Cacheable.java
@@ -17,13 +17,55 @@ package io.jboot.components.cache.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * @author michael yang
+ */
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
 public @interface Cacheable {
+
+    /**
+     * 缓存名称
+     *
+     * @return
+     */
     String name();
+
+    /**
+     * 缓存的key
+     *
+     * @return
+     */
     String key() default "";
+
+    /**
+     * 缓存时间，单位秒
+     * 默认情况下，缓存永久有效
+     *
+     * @return
+     */
     int liveSeconds() default 0; // 0，系统配置默认，默认情况下永久有效
+
+    /**
+     * 是否对 null 值进行缓存
+     *
+     * @return
+     */
     boolean nullCacheEnable() default false;
+
+    /**
+     * 是否对返回的数据进行 Copy 后返回
+     *
+     * @return
+     */
+    boolean returnCopyEnable() default false;
+
+    /**
+     * 在什么情况下不进行缓存
+     * 这里编写的是 JFinal 模板引擎的表达式
+     *
+     * @return
+     */
     String unless() default "";
 }
