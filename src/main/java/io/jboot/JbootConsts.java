f83ee7f82bf840b1edcf2d7aa44c5c20c6b1dfe5
yangfuhai <fuhai999@gmail.com>
2020-01-13 10:40:01 +0800

    v3.0.0-rc.1 release (^.^)YYa!!

--- src/main/java/io/jboot/JbootConsts.java
+++ src/main/java/io/jboot/JbootConsts.java
@@ -22,7 +22,7 @@ package io.jboot;
  */
 public class JbootConsts {
 
-    public static String VERSION = "2.3.3";
+    public static String VERSION = "3.0.0-rc.1";
 
 
     public static final String ATTR_REQUEST = "REQUEST";
