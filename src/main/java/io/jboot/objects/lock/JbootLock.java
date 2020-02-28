74b12be3a735a7d8f2981a7a8d3e67a6c954e3fa
yangfuhai <fuhai999@gmail.com>
2020-02-28 17:02:07 +0800

    add jboot objects

---
+++ src/main/java/io/jboot/objects/lock/JbootLock.java
@@ -0,0 +1,8 @@
+package io.jboot.objects.lock;
+
+/**
+ * @author michael yang (fuhai999@gmail.com)
+ * @Date: 2020/2/28
+ */
+public interface JbootLock {
+}
---
+++ src/main/java/io/jboot/objects/multimap/JbootMultimap.java
@@ -0,0 +1,8 @@
+package io.jboot.objects.multimap;
+
+/**
+ * @author michael yang (fuhai999@gmail.com)
+ * @Date: 2020/2/28
+ */
+public interface JbootMultimap {
+}
