74d646c2b5dfa7f7d992afae47a3bd90c0877dd3
wangjun <wjun_java@163.com>
2020-04-10 17:26:29 +0800

    jwt增加可以从request里面获取jwt

--- src/main/java/io/jboot/support/jwt/JwtInterceptor.java
+++ src/main/java/io/jboot/support/jwt/JwtInterceptor.java
@@ -44,8 +44,7 @@ public class JwtInterceptor implements FixedInterceptor {
         HttpServletRequest request = inv.getController().getRequest();
         String token = request.getHeader(JwtManager.me().getHttpHeaderName());
 
-        if(StrUtil.isBlank(token))
-            token = request.getParameter(JwtManager.me().getHttpHeaderName());
+        if(StrUtil.isBlank(token)) token = request.getParameter("token");
 
         if (StrUtil.isBlank(token)) {
             processInvoke(inv, null);
