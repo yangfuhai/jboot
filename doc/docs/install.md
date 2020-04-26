cc4c27e672b8c9c664037567b2c7039ddc63f678
yangfuhai <fuhai999@gmail.com>
2020-04-26 17:44:30 +0800

    v3.1.9 release (^.^)YYa!!

--- doc/docs/install.md
+++ doc/docs/install.md
@@ -9,7 +9,7 @@
 <dependency>
     <groupId>io.jboot</groupId>
     <artifactId>jboot</artifactId>
-    <version>3.1.8</version>
+    <version>3.1.9</version>
 </dependency>
 ```
 
--- src/main/java/io/jboot/JbootConsts.java
+++ src/main/java/io/jboot/JbootConsts.java
@@ -22,7 +22,7 @@ package io.jboot;
  */
 public class JbootConsts {
 
-    public static String VERSION = "3.1.8";
+    public static String VERSION = "3.1.9";
 
 
     public static final String ATTR_REQUEST = "REQUEST";
