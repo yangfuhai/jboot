04371ad0815c27b102d2535ccbf7f9747f2f84b5
yangfuhai <fuhai999@gmail.com>
2020-07-28 16:03:28 +0800

    add seata tcc support

--- src/main/java/io/jboot/support/seata/tcc/TccActionInterceptor.java
+++ src/main/java/io/jboot/support/seata/tcc/TccActionInterceptor.java
@@ -17,16 +17,8 @@ package io.jboot.support.seata.tcc;
 
 import com.jfinal.aop.Interceptor;
 import com.jfinal.aop.Invocation;
-import com.jfinal.log.Log;
 import io.jboot.support.seata.JbootSeataManager;
 import io.jboot.web.fixedinterceptor.FixedInterceptor;
-import io.seata.common.util.StringUtils;
-import io.seata.core.context.RootContext;
-import io.seata.core.model.BranchType;
-import io.seata.rm.tcc.api.TwoPhaseBusinessAction;
-import io.seata.rm.tcc.remoting.RemotingDesc;
-
-import java.lang.reflect.Method;
 
 
 /**
@@ -38,30 +30,6 @@ import java.lang.reflect.Method;
  */
 public class TccActionInterceptor implements Interceptor, FixedInterceptor {
 
-    private static final Log LOGGER = Log.getLog(TccActionInterceptor.class);
-
-    private ActionInterceptorHandler actionInterceptorHandler = new ActionInterceptorHandler();
-
-    /**
-     * remoting bean info
-     */
-    protected RemotingDesc remotingDesc;
-
-    /**
-     * Instantiates a new Tcc action interceptor.
-     */
-    public TccActionInterceptor() {
-
-    }
-
-    /**
-     * Instantiates a new Tcc action interceptor.
-     *
-     * @param remotingDesc the remoting desc
-     */
-    public TccActionInterceptor(RemotingDesc remotingDesc) {
-        this.remotingDesc = remotingDesc;
-    }
 
     @Override
     public void intercept(Invocation inv) {
@@ -71,36 +39,7 @@ public class TccActionInterceptor implements Interceptor, FixedInterceptor {
             return;
         }
 
-        if (!RootContext.inGlobalTransaction()) {
-            // not in transaction
-            inv.invoke();
-            return;
-        }
-        Method method = inv.getMethod();
-        TwoPhaseBusinessAction businessAction = method.getAnnotation(TwoPhaseBusinessAction.class);
-        //try method
-        if (businessAction != null) {
-            //save the xid
-            String xid = RootContext.getXID();
-            //clear the context
-            String previousBranchType = RootContext.getBranchType();
-            RootContext.bindBranchType(BranchType.TCC);
-            try {
-                Object[] methodArgs = inv.getArgs();
-                //Handler the TCC Aspect
-                actionInterceptorHandler.proceed(method, methodArgs, xid, businessAction, inv);
-                return;
-            } finally {
-                RootContext.unbindBranchType();
-                //restore the TCC branchType if exists
-                if (StringUtils.equals(BranchType.TCC.name(), previousBranchType)) {
-                    RootContext.bindBranchType(BranchType.TCC);
-                }
-            }
-        } else {
-            inv.invoke();
-        }
-        return;
+        new TccActionProcesser().intercept(inv);
     }
 
 }
---
+++ src/main/java/io/jboot/support/seata/tcc/TccActionProcesser.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2015-2020, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.jboot.support.seata.tcc;
+
+import com.jfinal.aop.Invocation;
+import io.seata.common.util.StringUtils;
+import io.seata.core.context.RootContext;
+import io.seata.core.model.BranchType;
+import io.seata.rm.tcc.api.TwoPhaseBusinessAction;
+
+import java.lang.reflect.Method;
+
+
+/**
+ * TCC Interceptor
+ * <p>
+ * 参考： https://github.com/seata/seata/blob/develop/spring/src/main/java/io/seata/spring/tcc/TccActionInterceptor.java
+ *
+ * @author zhangsen
+ */
+public class TccActionProcesser {
+
+
+    private ActionInterceptorHandler actionInterceptorHandler = new ActionInterceptorHandler();
+
+
+
+    public void intercept(Invocation inv) {
+
+        if (!RootContext.inGlobalTransaction()) {
+            // not in transaction
+            inv.invoke();
+            return;
+        }
+        Method method = inv.getMethod();
+        TwoPhaseBusinessAction businessAction = method.getAnnotation(TwoPhaseBusinessAction.class);
+        //try method
+        if (businessAction != null) {
+            //save the xid
+            String xid = RootContext.getXID();
+            //clear the context
+            String previousBranchType = RootContext.getBranchType();
+            RootContext.bindBranchType(BranchType.TCC);
+            try {
+                Object[] methodArgs = inv.getArgs();
+                //Handler the TCC Aspect
+                actionInterceptorHandler.proceed(method, methodArgs, xid, businessAction, inv);
+            } finally {
+                RootContext.unbindBranchType();
+                //restore the TCC branchType if exists
+                if (StringUtils.equals(BranchType.TCC.name(), previousBranchType)) {
+                    RootContext.bindBranchType(BranchType.TCC);
+                }
+            }
+        } else {
+            inv.invoke();
+        }
+    }
+
+}
