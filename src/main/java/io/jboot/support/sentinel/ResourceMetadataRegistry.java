cc6a5c9f8ba42690eb79676bb7fa5edb9b3a0bfb
yangfuhai <fuhai999@gmail.com>
2020-09-22 18:05:26 +0800

    optimize

--- src/main/java/io/jboot/support/sentinel/ResourceMetadataRegistry.java
+++ src/main/java/io/jboot/support/sentinel/ResourceMetadataRegistry.java
@@ -66,7 +66,7 @@ final class ResourceMetadataRegistry {
     }
 
     private static String getKey(Class<?> clazz, String name) {
-        return String.format("%s:%s", clazz.getCanonicalName(), name);
+        return clazz.getCanonicalName() + ":" + name;
     }
 
     /**
