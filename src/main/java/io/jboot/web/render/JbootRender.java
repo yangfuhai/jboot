80b11c71546274b0adca53fa2918e230e495258e
yangfuhai <fuhai999@gmail.com>
2020-09-24 17:44:49 +0800

    optimize

--- src/main/java/io/jboot/web/render/JbootRender.java
+++ src/main/java/io/jboot/web/render/JbootRender.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015-2016, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * Copyright (c) 2015-2020, Michael Yang 杨福海 (fuhai999@gmail.com).
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,20 +16,22 @@
 package io.jboot.web.render;
 
 import com.jfinal.render.Render;
+import com.jfinal.render.RenderException;
 import com.jfinal.render.RenderManager;
 import com.jfinal.template.Engine;
 import io.jboot.Jboot;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
 public class JbootRender extends Render {
 
-
     private static Engine engine;
-
     private static final String contentType = "text/html; charset=" + getEncoding();
+    private static JbootWebCdnConfig cdnConfig = Jboot.config(JbootWebCdnConfig.class);
 
     private Engine getEngine() {
         if (engine == null) {
@@ -38,11 +40,8 @@ public class JbootRender extends Render {
         return engine;
     }
 
-    private JbootWebCdnConfig config;
-
     public JbootRender(String view) {
         this.view = view;
-        this.config = Jboot.config(JbootWebCdnConfig.class);
     }
 
     public String getContentType() {
@@ -59,10 +58,35 @@ public class JbootRender extends Render {
             data.put(attrName, request.getAttribute(attrName));
         }
 
-        String html = getEngine().getTemplate(view).renderToString(data);
-        html = config.isEnable() ? RenderHelpler.processCDN(html, config.getDomain()) : html;
+        if (cdnConfig.isEnable()){
+            renderWithCdnConfig(data);
+        }else {
+            renderNormal(data);
+        }
+    }
 
-        RenderHelpler.renderHtml(response, html, contentType);
+    private void renderWithCdnConfig(Map<Object, Object> data){
+        String html = getEngine().getTemplate(view).renderToString(data);
+        RenderHelpler.renderHtml(response, RenderHelpler.processCDN(html, cdnConfig.getDomain()) , contentType);
+    }
+
+    private void renderNormal(Map<Object, Object> data){
+        try {
+            OutputStream os = response.getOutputStream();
+            engine.getTemplate(view).render(data, os);
+            os.flush();
+        } catch (RuntimeException e) {	// 捕获 ByteWriter.close() 抛出的 RuntimeException
+            Throwable cause = e.getCause();
+            if (cause instanceof IOException) {	// ClientAbortException、EofException 直接或间接继承自 IOException
+                String name = cause.getClass().getSimpleName();
+                if ("ClientAbortException".equals(name) || "EofException".equals(name)) {
+                    return ;
+                }
+            }
+            throw e;
+        } catch (IOException e) {
+            throw new RenderException(e);
+        }
     }
 
 
