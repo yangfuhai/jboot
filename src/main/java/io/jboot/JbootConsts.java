7850e220a9e79817c00b1ce1e46d5c26a5193eba
yangfuhai <fuhai999@gmail.com>
2020-11-29 16:14:47 +0800

    v3.6.4 release (^.^)YYa!!

--- src/main/java/io/jboot/JbootConsts.java
+++ src/main/java/io/jboot/JbootConsts.java
@@ -22,7 +22,7 @@ package io.jboot;
  */
 public class JbootConsts {
 
-    public static String VERSION = "3.6.3";
+    public static String VERSION = "3.6.4";
 
 
     public static final String ATTR_REQUEST = "REQUEST";
