8b288c88a7baf6fa9fcca674a7be44cd7f1220e9
yangfuhai <fuhai999@gmail.com>
2020-12-03 18:10:43 +0800

    add cache time for JbootAccessTokenCache

--- src/main/java/io/jboot/components/cache/support/JbootAccessTokenCache.java
+++ src/main/java/io/jboot/components/cache/support/JbootAccessTokenCache.java
@@ -2,6 +2,7 @@ package io.jboot.components.cache.support;
 
 import com.jfinal.weixin.sdk.cache.IAccessTokenCache;
 import io.jboot.Jboot;
+import io.jboot.components.cache.CacheTime;
 
 public class JbootAccessTokenCache implements IAccessTokenCache {
 
@@ -14,7 +15,8 @@ public class JbootAccessTokenCache implements IAccessTokenCache {
 
     @Override
     public void set(String key, String value) {
-        Jboot.getCache().put(CACHE_NAME, key, value);
+        // 微信相关 token 的有效期之多 2 个小时
+        Jboot.getCache().put(CACHE_NAME, key, value,2 * CacheTime.HOUR);
     }
 
     @Override
