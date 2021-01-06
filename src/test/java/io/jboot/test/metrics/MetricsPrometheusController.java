492a24adde3cc284e899a6af6361c53503fb6c9c
yangfuhai <fuhai999@gmail.com>
2021-01-06 18:31:13 +0800

    add prometheus reporter

---
+++ src/test/java/io/jboot/test/metrics/MetricsPrometheusController.java
@@ -0,0 +1,29 @@
+package io.jboot.test.metrics;
+
+import io.jboot.app.JbootApplication;
+import io.jboot.support.metric.annotation.*;
+import io.jboot.web.controller.JbootController;
+import io.jboot.web.controller.annotation.RequestMapping;
+
+@RequestMapping("/metrics/prometheus")
+public class MetricsPrometheusController extends JbootController {
+
+    public static void main(String[] args) {
+        JbootApplication.setBootArg("jboot.metric.url", "/metrics_admin");
+        JbootApplication.setBootArg("jboot.metric.reporter", "prometheus");
+
+//        JbootApplication.setBootArg("jboot.metric.reporter.prometheus.host", "127.0.0.1");
+//        JbootApplication.setBootArg("jboot.metric.reporter.prometheus.port", "1234");
+
+        JbootApplication.run(args);
+    }
+
+    @EnableMetricCounter
+    @EnableMetricConcurrency
+    @EnableMetricTimer
+    @EnableMetricHistogram
+    @EnableMetricMeter
+    public void index() {
+        renderText("metrics prometheus index. ");
+    }
+}
