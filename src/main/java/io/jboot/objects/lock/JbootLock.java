b81957d1395c7761b556cdfbc9e6cae5668a53c7
yangfuhai <fuhai999@gmail.com>
2021-02-12 18:23:43 +0800

    v3.9.3 snapshot

--- src/main/java/io/jboot/objects/lock/JbootLock.java
+++ src/main/java/io/jboot/objects/lock/JbootLock.java
@@ -1,5 +1,6 @@
 package io.jboot.objects.lock;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 
 /**
@@ -7,4 +8,16 @@ import java.util.concurrent.locks.Lock;
  * @Date: 2020/2/28
  */
 public interface JbootLock extends Lock {
+
+    @Override
+    void lock();
+
+    @Override
+    void unlock();
+
+    @Override
+    boolean tryLock();
+
+    @Override
+    boolean tryLock(long time, TimeUnit unit) throws InterruptedException;
 }
