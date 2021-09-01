e9dc3c047dd7786aed2b50f281148ec4f63ab3e2
yangfuhai <fuhai999@gmail.com>
2021-09-01 10:43:28 +0800

    v3.11.0 snapshot

--- src/main/java/io/jboot/web/PathVariableActionMapping.java
+++ src/main/java/io/jboot/web/PathVariableActionMapping.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2015-2021, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.jboot.web;
 
 import com.google.common.base.Joiner;
@@ -18,6 +33,10 @@ import java.lang.reflect.Type;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * @author 没牙的小朋友 （mjl@nxu.edu.cn）
+ * @version V1.0
+ */
 public class PathVariableActionMapping extends JbootActionMapping {
     private static final String PATH_VARIABLE_URL_PATTERN = ".*\\{[a-zA-Z0-9]+\\}.*";
     protected Map<String, Action> pathVariableUrlMapping = new ConcurrentHashMap<>();
