b0808e9c90ba7ea11985234feaafe45f6203a8b6
orangej <chenchi1985@126.com>
2021-09-09 10:38:17 +0800

    完善shiro单元测试

--- src/test/java/io/jboot/test/shiro/ShiroController.java
+++ src/test/java/io/jboot/test/shiro/ShiroController.java
@@ -3,12 +3,16 @@ package io.jboot.test.shiro;
 
 import io.jboot.web.controller.JbootController;
 import io.jboot.web.controller.annotation.RequestMapping;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.authz.annotation.RequiresGuest;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.apache.shiro.subject.Subject;
 
-@RequestMapping(value = "/shiro",viewPath = "/htmls/shiro")
+@RequestMapping(value = "/shiro", viewPath = "/htmls/shiro")
 public class ShiroController extends JbootController {
 
 
@@ -16,37 +20,56 @@ public class ShiroController extends JbootController {
         renderText("index");
     }
 
-    public void login(){
+    public void login() {
         renderText("login");
     }
 
-    public void doLogin(){
-
+    public void doLogin() {
         Subject subject = SecurityUtils.getSubject();
-        subject.login(new TestAuthenticationToken());
+        // 默认为admin登陆
+        UsernamePasswordToken token = new UsernamePasswordToken(getPara("username", "admin"), "123");
+        subject.login(token);
 
 //        subject.isAuthenticated();
 //        subject.isPermitted()
 
         renderText("logined success");
-
     }
 
-    public void logout(){
+    public void logout() {
         Subject subject = SecurityUtils.getSubject();
         subject.logout();
         renderText("logouted success");
     }
 
     @RequiresAuthentication
-    public void usercenter(){
+    public void usercenter() {
         renderText("usercenter");
     }
 
 
     @RequiresGuest
-    public void guest(){
+    public void guest() {
         renderText("guest");
     }
 
+    @RequiresRoles("editor")
+    public void editor() {
+        renderText("editor");
+    }
+
+    @RequiresRoles("admin")
+    public void admin() {
+        renderText("admin");
+    }
+
+    @RequiresPermissions("all:read")
+    public void readAll() {
+        renderText("all");
+    }
+
+    @RequiresPermissions("news:read")
+    public void readNews() {
+        renderText("news");
+    }
 }
