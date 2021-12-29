71e4f0c7b9bac97a541469de85ea902b8dae1051
yangfuhai <fuhai999@gmail.com>
2021-12-29 16:01:31 +0800

    v3.13.1 release (^.^)YYa!!

--- src/main/java/io/jboot/JbootConsts.java
+++ src/main/java/io/jboot/JbootConsts.java
@@ -22,7 +22,7 @@ package io.jboot;
  */
 public class JbootConsts {
 
-    public static String VERSION = "3.13.0";
+    public static String VERSION = "3.13.1";
 
 
     public static final String ATTR_REQUEST = "REQUEST";
