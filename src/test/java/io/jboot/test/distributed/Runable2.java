334c762add1cf122be74fd10ccdd0bbc3c32335e
开源海哥 <fuhai999@gmail.com>
2022-04-27 09:24:40 +0800

    update redis mq test

--- src/test/java/io/jboot/test/distributed/Runable2.java
+++ src/test/java/io/jboot/test/distributed/Runable2.java
@@ -1,17 +1,17 @@
-package io.jboot.test.distributed;
-
-import io.jboot.components.schedule.annotation.EnableDistributedRunnable;
-import io.jboot.components.schedule.annotation.FixedDelay;
-
-@FixedDelay(period = 30)
-@EnableDistributedRunnable(redisKey = "aaa",expireSeconds = 30)
-public class Runable2 implements Runnable {
-    int i =0;
-    @Override
-    public void run() {
-        if (i++ >= 3){
-            throw new IllegalStateException("messss...");
-        }
-        System.err.println("Runable2.running.........."+this);
-    }
-}
+//package io.jboot.test.distributed;
+//
+//import io.jboot.components.schedule.annotation.EnableDistributedRunnable;
+//import io.jboot.components.schedule.annotation.FixedDelay;
+//
+//@FixedDelay(period = 30)
+//@EnableDistributedRunnable(redisKey = "aaa",expireSeconds = 30)
+//public class Runable2 implements Runnable {
+//    int i =0;
+//    @Override
+//    public void run() {
+//        if (i++ >= 3){
+//            throw new IllegalStateException("messss...");
+//        }
+//        System.err.println("Runable2.running.........."+this);
+//    }
+//}
--- src/test/java/io/jboot/test/mq/redis/RedisMqSender.java
+++ src/test/java/io/jboot/test/mq/redis/RedisMqSender.java
@@ -4,8 +4,6 @@ package io.jboot.test.mq.redis;
 import io.jboot.Jboot;
 import io.jboot.app.JbootApplication;
 
-import java.util.UUID;
-
 public class RedisMqSender {
 
     public static void main(String[] args) throws InterruptedException {
@@ -17,28 +15,30 @@ public class RedisMqSender {
         JbootApplication.setBootArg("jboot.mq.type", "redis");
         JbootApplication.setBootArg("jboot.mq.redis.host", "127.0.0.1");
 
-//        JbootApplication.setBootArg("jboot.mq.type", "redis");
-//        JbootApplication.setBootArg("jboot.mq.redis.host", "127.0.0.1");
+        JbootApplication.setBootArg("jboot.mq.type", "redis");
+        JbootApplication.setBootArg("jboot.mq.redis.host", "127.0.0.1");
 
-        JbootApplication.setBootArg("jboot.mq.other1.type", "rocketmq");
-        JbootApplication.setBootArg("jboot.mq.other1.channel", "channel1,channel2,myChannel");
-        JbootApplication.setBootArg("jboot.mq.other1.typeName", "rktmq");
-        JbootApplication.setBootArg("jboot.mq.rocket.rktmq.namesrvAddr", "127.0.0.1:9876");
+//        JbootApplication.setBootArg("jboot.mq.other1.type", "rocketmq");
+//        JbootApplication.setBootArg("jboot.mq.other1.channel", "channel1,channel2,myChannel");
+//        JbootApplication.setBootArg("jboot.mq.other1.typeName", "rktmq");
+//        JbootApplication.setBootArg("jboot.mq.rocket.rktmq.namesrvAddr", "127.0.0.1:9876");
 
         //启动应用程序
         JbootApplication.run(args);
 
-//        while (true) {
+        int count = 10;
+        for (int i = 0; i < count; i++) {
 
-            Jboot.getMq().publish("message from RedisMqSender", "channel1");
-            Jboot.getMq().publish("message from RedisMqSender", "channel2");
-            Jboot.getMq().publish("message from RedisMqSender", "myChannel");
 
-            Jboot.getMq("other1").enqueue("message from RedisMqSender by enqueue : " + UUID.randomUUID(), "channel1");
+//            Jboot.getMq().publish("message from RedisMqSender", "channel1");
+//            Jboot.getMq().publish("message from RedisMqSender", "channel2");
+//            Jboot.getMq().publish("message from RedisMqSender", "myChannel");
 
-            Thread.sleep(2000);
+            Jboot.getMq().enqueue("message " + i, "channel1");
+
+//            Thread.sleep(2000);
             System.out.println("jboot mq publish success...");
-//        }
+        }
 
     }
 
