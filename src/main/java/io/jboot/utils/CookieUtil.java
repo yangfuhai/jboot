04d5d417e998524a95f406afe4fbc6aa5621b6c9
开源海哥 <fuhai999@gmail.com>
2022-06-21 17:03:07 +0800

    optimize CookieUtil

--- src/main/java/io/jboot/utils/CookieUtil.java
+++ src/main/java/io/jboot/utils/CookieUtil.java
@@ -19,9 +19,15 @@ import com.jfinal.core.Controller;
 import com.jfinal.kit.Base64Kit;
 import com.jfinal.kit.HashKit;
 import com.jfinal.kit.LogKit;
+import com.jfinal.kit.StrKit;
 import com.jfinal.log.Log;
+import io.jboot.Jboot;
+import io.jboot.exception.JbootIllegalConfigException;
 import io.jboot.web.JbootWebConfig;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.math.BigInteger;
 
 /**
@@ -60,6 +66,11 @@ public class CookieUtil {
         COOKIE_ENCRYPT_KEY = key;
     }
 
+
+    public static String getEncryptKey() {
+        return COOKIE_ENCRYPT_KEY;
+    }
+
     /**
      * 设置 默认的 Cookie 有效时间，单位：秒
      *
@@ -67,6 +78,10 @@ public class CookieUtil {
      */
     public static void initDefaultCookieMaxAge(int seconds) {
         COOKIE_MAX_AGE = seconds;
+    }
+
+    public static int getDefaultCookieMaxAge() {
+        return COOKIE_MAX_AGE;
     }
 
 
@@ -90,6 +105,10 @@ public class CookieUtil {
         put(ctr, key, value, COOKIE_MAX_AGE, defaultPath, defaultDomain, COOKIE_ENCRYPT_KEY);
     }
 
+    public static void put(HttpServletResponse response, String key, Object value) {
+        put(response, key, value, COOKIE_MAX_AGE, defaultPath, defaultDomain, COOKIE_ENCRYPT_KEY);
+    }
+
 
     public static void put(Controller ctr, String key, Object value, String secretKey) {
         put(ctr, key, value, COOKIE_MAX_AGE, defaultPath, defaultDomain, secretKey);
@@ -106,25 +125,40 @@ public class CookieUtil {
 
 
     public static void put(Controller ctr, String key, Object value, int maxAgeInSeconds, String path, String domain, String secretKey) {
-        String cookie = buildCookieValue(value.toString(), maxAgeInSeconds, secretKey);
-        ctr.setCookie(key, cookie, maxAgeInSeconds, path, domain, true);
+        put(ctr.getResponse(), key, value, maxAgeInSeconds, path, domain, secretKey);
     }
 
+    public static void put(HttpServletResponse response, String key, Object value, int maxAgeInSeconds, String path, String domain, String secretKey) {
+        String cookie = buildCookieValue(value.toString(), maxAgeInSeconds, secretKey);
+        doSetCookie(response, key, cookie, maxAgeInSeconds, path, domain, true);
+    }
 
     public static void remove(Controller ctr, String key) {
-        ctr.removeCookie(key);
+        doSetCookie(ctr.getResponse(), key, null, 0, null, null, null);
+    }
+
+    public static void remove(HttpServletResponse response, String key) {
+        doSetCookie(response, key, null, 0, null, null, null);
     }
 
     public static void remove(Controller ctr, String key, String path, String domain) {
-        ctr.removeCookie(key, path, domain);
+        doSetCookie(ctr.getResponse(), key, null, 0, path, domain, null);
     }
 
     public static String get(Controller ctr, String key) {
         return get(ctr, key, COOKIE_ENCRYPT_KEY);
     }
 
+    public static String get(HttpServletRequest request, String key) {
+        return get(request, key, COOKIE_ENCRYPT_KEY);
+    }
+
     public static String get(Controller ctr, String key, String secretKey) {
-        String cookieValue = ctr.getCookie(key);
+        return get(ctr.getRequest(), key, secretKey);
+    }
+
+    public static String get(HttpServletRequest request, String key, String secretKey) {
+        String cookieValue = getCookie(request, key, null);
 
         if (cookieValue == null) {
             return null;
@@ -161,8 +195,17 @@ public class CookieUtil {
     }
 
     private static String encrypt(String secretKey, Object saveTime, Object maxAgeInSeconds, String value) {
+
+        // 若使用了默认的加密秘钥
         if (JbootWebConfig.DEFAULT_COOKIE_ENCRYPT_KEY.equals(secretKey)) {
-            LOG.warn("warn!!! encrypt key is defalut value. please config \"jboot.web.cookieEncryptKey = xxx\" in jboot.properties ");
+
+            if (Jboot.isDevMode()) {
+                LOG.warn("Warn!!! encrypt key is defalut value. please config \"jboot.web.cookieEncryptKey = xxx\" in jboot.properties");
+            }
+            // 生产环境下，直接抛出错误!
+            else {
+                throw new JbootIllegalConfigException("Error!!! Cookie encrypt key is not configured. please config \"jboot.web.cookieEncryptKey = xxx\" in jboot.properties");
+            }
         }
         return HashKit.md5(secretKey + saveTime.toString() + maxAgeInSeconds.toString() + value);
     }
@@ -237,5 +280,46 @@ public class CookieUtil {
         String value = get(ctr, key);
         return null == value ? defalut : new BigInteger(value);
     }
+
+
+    private static String getCookie(HttpServletRequest request, String name, String defaultValue) {
+        Cookie cookie = getCookieObject(request, name);
+        return cookie != null ? cookie.getValue() : defaultValue;
+    }
+
+    /**
+     * Get cookie object by cookie name.
+     */
+    private static Cookie getCookieObject(HttpServletRequest request, String name) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(name)) {
+                    return cookie;
+                }
+            }
+        }
+        return null;
+    }
+
+
+    private static void doSetCookie(HttpServletResponse response, String name, String value, int maxAgeInSeconds, String path, String domain, Boolean isHttpOnly) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setMaxAge(maxAgeInSeconds);
+        // set the default path value to "/"
+        if (StrKit.isBlank(path)) {
+            path = "/";
+        }
+        cookie.setPath(path);
+
+        if (domain != null) {
+            cookie.setDomain(domain);
+        }
+        if (isHttpOnly != null) {
+            cookie.setHttpOnly(isHttpOnly);
+        }
+        response.addCookie(cookie);
+    }
+
 
 }
