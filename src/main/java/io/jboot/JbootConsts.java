32bcf224273a97e28fb8d679c6e70d7727797d02
开源海哥 <fuhai999@gmail.com>
2022-08-07 17:33:02 +0800

    v3.15.8 release (^.^)YYa!!

--- src/main/java/io/jboot/JbootConsts.java
+++ src/main/java/io/jboot/JbootConsts.java
@@ -22,7 +22,7 @@ package io.jboot;
  */
 public class JbootConsts {
 
-    public static String VERSION = "3.15.7";
+    public static String VERSION = "3.15.8";
 
 
     public static final String ATTR_CONTEXT_PATH = "CPATH";
